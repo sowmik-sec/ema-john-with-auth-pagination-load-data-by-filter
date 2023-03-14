b0c3eb2599b24085ffc1c0224421a061ace5d718
sowmik-sec <sowmik.sec@gmail.com>
2023-03-14 12:23:52 +0600

    use post to load some products using keys

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -44,15 +44,27 @@ const Shop = () => {
   useEffect(() => {
     const storedCart = getStoredCart();
     const savedCart = [];
-    for (const id in storedCart) {
-      const addedProduct = products.find((product) => product._id === id);
-      if (addedProduct) {
-        const quantity = storedCart[id];
-        addedProduct.quantity = quantity;
-        savedCart.push(addedProduct);
-      }
-    }
-    setCart(savedCart);
+    const ids = Object.keys(storedCart);
+    console.log(ids);
+    fetch("http://localhost:5000/productsByIds", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(ids),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        for (const id in storedCart) {
+          const addedProduct = data.find((product) => product._id === id);
+          if (addedProduct) {
+            const quantity = storedCart[id];
+            addedProduct.quantity = quantity;
+            savedCart.push(addedProduct);
+          }
+        }
+        setCart(savedCart);
+      });
   }, [products]);
 
   const handleAddToCart = (selectedProduct) => {
@@ -102,7 +114,7 @@ const Shop = () => {
             onClick={() => setPage(number)}
             key={number}
           >
-            {number}
+            {number + 1}
           </button>
         ))}
         <select onChange={(event) => setSize(event.target.value)}>
